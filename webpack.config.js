1c94e3b4ad8f85ca5705815cd018a2a590c26190
markocen <markocen@gmail.com>
2016-11-17 14:54:50 -0500

    add bundle

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        "benthos": "./build/benthos.js",
+        "benthos.min": "./build/benthos.js",
+    },
+    output: {
+        filename: '[name].js',
+        path: 'dist/',
+        sourceMapFilename: '[file].map',
+        library: 'benthos'
+    },
+    devtool: "source-map",
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            minimize: true
+        })
+    ]
+};
