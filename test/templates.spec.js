6b98d3c6dba55a9d07734090705d87ede402ebb5
markocen <markocen@gmail.com>
2016-11-18 12:42:00 -0500

    update test cases

--- test/templates.spec.js
+++ test/templates.spec.js
@@ -17,11 +17,46 @@ describe('templates', ()=>{
             ])
         })
 
-        it('could access the objects in scope', ()=>{
-            expect(benthos.compile('Gender is #{gender(val)}', { val: true })).to.be.oneOf([
-                'Gender is 0',
-                'Gender is 1'
-            ])
+        it('should support scope variables and functions', ()=>{
+            let template = benthos.compile('#{hisName("Shan")} is #{gender(val)}', {
+                val: true,
+                hisName: function (lastName) {
+                    return `${this.firstName(1)} ${lastName}`
+                }
+            });
+            expect(template).to.match(/^\w*\sShan\sis\s[0-1]/)
+        })
+    })
+
+    describe('schema()', ()=>{
+        it('should interpret template ', ()=>{
+            let User = benthos.schema({
+                age: '#{integer(20, 25)}',
+                gender: '#{gender}',
+                name: 'Smith'
+            });
+            let user = User();
+            expect(user.age).to.be.within(20, 25);
+            expect(user.gender).to.be.oneOf(['Male', 'Female']);
+            expect(user.name).to.equal('Smith');
+        })
+
+        it('should support scope variables and functions', ()=>{
+            let User = benthos.schema({
+                age: '#{twentyFive}',
+                name: '#{myName("Smith")}',
+                bio: 'I am from #{country}'
+            });
+            let user = User({
+                twentyFive: 25,
+                myName: function(firstName){
+                    return firstName + ' ' + this.lastName();
+                }
+            });
+            expect(user.age).to.be.within(20, 25);
+            expect(user.name).to.match(/^Smith\s\w*/);
+            expect(user.bio).to.match(/^I\sam\sfrom\s\w*/);
+
         })
     })
 });
--- test/utilities.spec.js
+++ test/utilities.spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const random = require('../build/utilities/random');
+const benthos = require('../build/benthos');
 const chai = require('chai');
 const expect = chai.expect;
 
@@ -20,6 +21,18 @@ describe('random', ()=>{
         it('should return a number from 10 to 10000', ()=>{
             expect(random.number(10, 10000)).be.within(10, 10000);
         })
+    })
+
+});
+
+describe('extend', ()=>{
+
+    it('should extend benthos methods', ()=>{
+        benthos.extend('five', () => {
+            return this.integer(0, 5);
+        })
+        let num = benthos.compile('#{five}');
+        expect(num).to.be.within(0, 5);
     })
 
 });
