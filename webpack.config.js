a5d75d6a3e27882fd1c49498842da10834de35d4
markocen <markocen@gmail.com>
2016-11-22 14:33:49 -0500

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,43 @@ module.exports = {
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
             include: /\.min\.js$/,
-            minimize: true,
-            mangle: true
+            mangle: {
+                toplevel: true,
+                sort: true,
+                eval: true,
+                properties: true
+            },
+            compress: {
+                warnings: false,
+                properties: true,
+                sequences: true,
+                dead_code: true,
+                conditionals: true,
+                comparisons: true,
+                evaluate: true,
+                booleans: true,
+                unused: true,
+                loops: true,
+                hoist_funs: true,
+                cascade: true,
+                if_return: true,
+                join_vars: true,
+                drop_debugger: true,
+                unsafe: true,
+                hoist_vars: true,
+                negate_iife: true,
+            },
+            output: {
+                space_colon: false,
+                comments: function(node, comment) {
+                    var text = comment.value;
+                    var type = comment.type;
+                    if (type == "comment2") {
+                        // multiline comment
+                        return /@copyright/i.test(text);
+                    }
+                }
+            }
         })
     ]
 };
